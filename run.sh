0d9dcc1468ff4fdec5ad2e9aa0f9ddfb151cbf93
Jake Goldman <jgoldman@caltech.edu>
2022-07-29 17:17:46 -0700

    add wamr step

--- run.sh
+++ run.sh
@@ -15,23 +15,41 @@ SIMD_IMPLEMENTATIONS="haswell westmere"
 PARSE_FILE=large-file.json
 N=5
 
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+
 if [[ "$wasm" = true ]]; then 
   echo "compiling parse.cpp to wasm target..."
-	emcc -O3 -o parse.wasm parse.cpp simdjson.cpp
+	emcc -O3 -o out/parse.wasm parse.cpp simdjson.cpp 2> /dev/null # ADD SIMDE SUPPORT
 
-
-
+  # Extraneous step - only for checking WASM code
+  echo "converting .wasm to .wat..."
+  ${WABT_PATH}/build/wasm2wat -o out/parse.wat out/parse.wasm
+  
+  if [[ "$simd" = true ]]; then 
+    echo "rebuilding WAMR with SIMD support..."
+    cd ${WAMR_PATH}/wamr-compiler/build 
+    cmake .. -DWAMR_BUILD_SIMD=1
+    make 
+    cd ${SCRIPT_PATH}
+  else 
+    echo "rebuilding WAMR without SIMD support..."
+    cd ${WAMR_PATH}/wamr-compiler/build 
+    cmake .. -DWAMR_BUILD_SIMD=0
+    make 
+    cd ${SCRIPT_PATH}
+  fi
 	# wamrc --enable-multi-thread -o test.aot test.wasm
 else
   echo "compiling parse.cpp to native target..."
-  g++ -O3 -o parse parse.cpp simdjson.cpp  
+  g++ -O3 -o out/parse parse.cpp simdjson.cpp  
 
   if [[ "$simd" = true ]]; then
     for imp in $SIMD_IMPLEMENTATIONS; do
       cp /dev/null results/native_${imp}.csv
       echo "testing $imp..."
       for i in $(seq $N); do 
-        ./parse ${imp} json-files/${PARSE_FILE} results/native_${imp}.csv
+        out/parse ${imp} json-files/${PARSE_FILE} results/native_${imp}.csv
       done
     done
 
@@ -39,7 +57,7 @@ else
     cp /dev/null results/native_fallback.csv
     echo "testing fallback..."
     for i in $(seq $N); do
-        ./parse fallback json-files/${PARSE_FILE} results/native_fallback.csv
+        out/parse fallback json-files/${PARSE_FILE} results/native_fallback.csv
     done
   fi
 fi
