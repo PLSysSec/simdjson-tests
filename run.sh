9c866634fdbc54a71d0b60f6348e99579a625581
Jake Goldman <jgoldman@caltech.edu>
2022-07-29 17:02:12 -0700

    optimize script

--- run.sh
+++ run.sh
@@ -16,31 +16,30 @@ PARSE_FILE=large-file.json
 N=5
 
 if [[ "$wasm" = true ]]; then 
+  echo "compiling parse.cpp to wasm target..."
+	emcc -O3 -o parse.wasm parse.cpp simdjson.cpp
 
-	emcc -o parse.wasm parse.cpp simdjson.cpp
+
 
 	# wamrc --enable-multi-thread -o test.aot test.wasm
-
-elif [[ "$simd" = true ]]; then
-  echo "compiling parse.cpp to native target..."
-  g++ -O3 -o parse parse.cpp simdjson.cpp
-
-  for imp in $SIMD_IMPLEMENTATIONS; do
-    cp /dev/null results/native_${imp}.csv
-    echo "testing $imp..."
-    for i in $(seq $N); do 
-      ./parse ${imp} json-files/${PARSE_FILE} results/native_${imp}.csv
-    done
-  done
-
 else
   echo "compiling parse.cpp to native target..."
   g++ -O3 -o parse parse.cpp simdjson.cpp  
 
-  cp /dev/null results/native_fallback.csv
-  echo "testing fallback..."
-  for i in $(seq $N)
-    do
-      ./parse fallback json-files/${PARSE_FILE} results/native_fallback.csv
+  if [[ "$simd" = true ]]; then
+    for imp in $SIMD_IMPLEMENTATIONS; do
+      cp /dev/null results/native_${imp}.csv
+      echo "testing $imp..."
+      for i in $(seq $N); do 
+        ./parse ${imp} json-files/${PARSE_FILE} results/native_${imp}.csv
+      done
     done
+
+  else
+    cp /dev/null results/native_fallback.csv
+    echo "testing fallback..."
+    for i in $(seq $N); do
+        ./parse fallback json-files/${PARSE_FILE} results/native_fallback.csv
+    done
+  fi
 fi
