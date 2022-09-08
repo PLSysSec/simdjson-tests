ee283f245cb892946640428c1f9fe9a1c63c9a9c
Jake Goldman <jgoldman@caltech.edu>
2022-09-07 20:13:49 -0400

    clean up parse.cpp

--- parse.cpp
+++ parse.cpp
@@ -13,12 +13,12 @@ int N;
 
 double parse(std::string_view json_sv) {
   struct timespec start, end;
-  double dt = 10.0;
+  double dt;
   clock_gettime(CLOCK_REALTIME, &start);
   
   ondemand::parser parser; 
   padded_string json = padded_string(json_sv);
-  ondemand::document tweets = parser.iterate(json);
+  ondemand::document doc = parser.iterate(json);
 
   clock_gettime(CLOCK_REALTIME, &end);
   dt = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / BILLION;
@@ -45,17 +45,15 @@ argv[3] number of iterations to test
 int main(int argc, char *argv[]) {
   set_implementation(argv[1]);
   std::string_view json_file{ argv[2] };
-  std::string N_s(argv[3]);
+  std::string N_str { argv[3] };
 
   std::string json_str;
   for (std::string line; std::getline(std::cin, line);) {
-      json_str += line;
+    json_str += line;
   }
   std::string_view json_sv { json_str };
-  std::cout << "Parsing: " << json_file << std::endl;
-  std::cout << "Character Count: " << json_str.length() << std::endl;
 
-  argv[3] ? sscanf(N_s.data(), "%d", &N) : N = 1;
+  argv[3] ? sscanf(N_str.data(), "%d", &N) : N = 1;
   for (int i = 0; i < N; i++) {
     double dt = parse(json_str);
     printf("%f\n", dt);
